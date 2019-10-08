447d08bfe1c3abd77cc09d4b54895e92d1087aec
opexa <nkatrandjiev@gmail.com>
2019-10-09 00:21:05 +0300

    Scaffold User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_210641) do
+ActiveRecord::Schema.define(version: 2019_10_08_211908) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
 
   create_table "workshops", force: :cascade do |t|
     t.datetime "created_at", null: false
