15dc52a5ba38ef156d8e1261093e916af3898312
opexa <nkatrandjiev@gmail.com>
2019-10-13 22:42:30 +0300

    Add a worker that will delete all past workshops

--- app/jobs/workshop_cleaner_job.rb
+++ app/jobs/workshop_cleaner_job.rb
@@ -1,7 +1,11 @@
 class WorkshopCleanerJob < ApplicationJob
   queue_as :default
 
+  after_perform do |job|
+    self.class.set(:wait => 10.minutes).perform_later
+  end
+
   def perform(*args)
-    puts "Background job DONE"
+    Workshop.where('date<?', Date.today).delete_all
   end
 end
