62d2fb57cb05299657387018aa5376c2297d20c7
opexa <nkatrandjiev@gmail.com>
2019-10-19 16:26:21 +0300

    Add foreign key to user in workshop

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_14_204829) do
+ActiveRecord::Schema.define(version: 2019_10_19_131156) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2019_10_14_204829) do
     t.datetime "updated_at", null: false
     t.date "date"
     t.string "title"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_workshops_on_user_id"
   end
 
 end
