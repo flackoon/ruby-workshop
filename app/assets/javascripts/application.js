c7554d3d9fd7d75edb72cb9a5c4aa2f89b057960
opexa <nkatrandjiev@gmail.com>
2019-10-22 00:24:51 +0300

    Visual improvements in the workshop show view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
+// = require rails-ujs
+// = require activestorage
 // = require turbolinks
 // = require_tree .
+// = require jquery
+// = require popper
+// = require bootstrap
+
+$(document).ready(function() {
+	$('[data-toggle="tooltip"]').tooltip()
+});
