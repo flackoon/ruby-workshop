ffc39bbf3809f784516056025f0c018aa7061f88
opexa <nkatrandjiev@gmail.com>
2019-10-29 21:55:32 +0200

    Remove user from comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_195341) do
+ActiveRecord::Schema.define(version: 2019_10_29_195430) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -20,10 +20,8 @@ ActiveRecord::Schema.define(version: 2019_10_29_195341) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "comments_workshops", id: false, force: :cascade do |t|
