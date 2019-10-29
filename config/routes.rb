f4096246d2546729ae63bad3ae6390ed8323db6f
opexa <nkatrandjiev@gmail.com>
2019-10-30 00:32:49 +0200

    Remove outdated routes and session controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root 'workshops#index'
   
   resources :account, only: [:index]
-  resources :sessions, only: [:new, :create, :destroy]
   resources :workshops do
     resources :participations
   end
@@ -12,9 +11,4 @@ Rails.application.routes.draw do
   get '/account/details/:id', to: 'account#show',  as: 'account'
   get '/account/edit', to: 'account#edit', as: 'edit_account'
   patch '/account/edit', to: 'account#update', as: 'update_account'
-  get '/account/register', to: 'account#register', as: 'register'
-  post '/account/register', to: 'account#create', as: 'create_account'
-  get '/account/login', to: 'sessions#new', as: 'login'
-  post '/account/login', to: 'sessions#create', as: 'login_post'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
