947063b38e01e7d5363f7b39cafd1ec30e4e511d
opexa <nkatrandjiev@gmail.com>
2019-11-01 00:21:13 +0200

    Add user to comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_201938) do
+ActiveRecord::Schema.define(version: 2019_10_31_221936) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2019_10_29_201938) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "comments_workshops", id: false, force: :cascade do |t|
