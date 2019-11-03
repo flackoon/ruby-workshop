f91907d35a73d8f5ecc95cce35b287a953dcedb3
opexa <nkatrandjiev@gmail.com>
2019-11-03 21:43:07 +0200

    wrap the sign in form with bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    redirect_to root_url
+  end
 end
